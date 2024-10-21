cf3bcd81d0e7cd7d792d06509e8625996b53943b
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-10-21 21:31:26 +0200

    add apples

---
+++ inc/apple.h
@@ -0,0 +1,40 @@
+#ifndef APPLE_H
+#define APPLE_H
+
+#include <SFML/Graphics/Color.hpp>
+#include <grid.h>
+#include <utility.h>
+
+namespace snake {
+
+struct apple {
+    static inline const sf::Color color{sf::Color::Red};
+
+    apple(sf::Vector2i position);
+
+    /**
+    * Draw the apple on a grid.
+    * 
+    * @parm grid Grid to draw the apple on.
+    */
+    template<std::size_t Rows, std::size_t Columns>
+    void draw(grid<Rows, Columns>& grid) const;
+
+    sf::Vector2i position{};
+};
+
+template<std::size_t Rows, std::size_t Columns>
+void apple::draw(grid<Rows, Columns>& grid) const {
+    sf::Vector2i index{position};
+
+    // map the position to a valid grid index
+    index.x = wrap<int>(index.x, 0, Rows - 1);
+    index.y = wrap<int>(index.y, 0, Columns - 1);
+
+    grid.fill(sf::Vector2<std::size_t>(index), apple::color);
+}
+
+}
+
+#endif
+
---
+++ src/apple.cpp
@@ -0,0 +1,9 @@
+#include <apple.h>
+
+namespace snake {
+
+apple::apple(sf::Vector2i position) 
+    :position{position} {}
+
+}
+
